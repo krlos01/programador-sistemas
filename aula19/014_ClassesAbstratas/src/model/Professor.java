4f7c306fd0bbe6879f7e6b3ad24e713406b73a34
krlos01 <44279252+krlos01@users.noreply.github.com>
2018-11-06 21:37:21 -0200

    Add files via upload

---
+++ aula19/014_ClassesAbstratas/src/model/Professor.java
@@ -0,0 +1,43 @@
+package model;
+public class Professor extends Usuario {
+	private String nome ;
+	private String especialidade;
+	private int matriculaProfessor;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}	
+	public String getEspecialidade() {
+		return especialidade;
+	}	
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}	
+	public int getMatriculaProfessor() {
+		return matriculaProfessor;
+	}	
+	public void setMatriculaProfessor(int matriculaProfessor) {
+		this.matriculaProfessor = matriculaProfessor;
+	}	
+	public Professor() {		
+		super();
+	}
+	public Professor(String login, String senha, int tipo, String nome,
+			String especialidade, int matriculaProfessor) {
+			super(login, senha, tipo);
+			this.nome = nome;
+			this.especialidade = especialidade;
+			this.matriculaProfessor = matriculaProfessor;
+	}			
+	@Override
+	public void mostrar() {
+		System.out.println("Informações do Professor: ");
+		System.out.println("Login: " + super.getLogin());
+		System.out.println("Senha: *****");
+		System.out.println("Matrícula: " + matriculaProfessor);
+		System.out.println("Nome: " + nome);
+		System.out.println("Especialidade: " + especialidade);
+	}
+}
---
+++ aula19/015_Interface/014_ClassesAbstratas/src/controller/Teste.java
@@ -0,0 +1,14 @@
+package controller;
+import model.*;
+public class Teste {
+	public static void main(String[] args) {
+		Professor prof = new Professor(
+				"joaquim", "joaquim123", 0, "Joaquim da Silva",
+				"Doutor", 1123);
+		prof.mostrar();
+		System.out.println("=================");
+		Aluno aluno = new Aluno("muzi", "muzi123", 1, "Sergio Muzi",
+				"primeiro", "tarde", 32115);
+		aluno.mostrar();
+	}
+}
